990bce56251e7e49f04c66cf2db7fcddca57f7dd
Manu Deva Arya <mdarya10@gmail.com>
2022-11-18 13:40:11 +0530

    - Resolving compilation error.

--- stop.c
+++ stop.c
@@ -9,7 +9,7 @@ static void __exit exitfunc(void)
 	cdev_del(&mydev->c_dev);
 	kfree(mydev);
 	/* extern void unregister_chrdev_region(dev_t, unsigned); */
-	unregister_chrdev_region(dev_id, num_dev);
+	unregister_chrdev_region(dev_id, nod);
 	
 	printk(KERN_INFO "%s: majorNo:%d \n", __func__, majorNo);
 	printk(KERN_INFO "%s: minorNo:%d \n", __func__, minorNo);
