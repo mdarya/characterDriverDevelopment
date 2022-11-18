0efb11725d1fb846a4fe33f2ca026d75ec0c60e2
Manu Deva Arya <mdarya10@gmail.com>
2022-11-18 19:29:56 +0530

    - Allocating memory for N number of devices - Initialising the cdev structures for all the devices and adding them   one by one to the device table.

--- stop.c
+++ stop.c
@@ -3,16 +3,20 @@
 
 static void __exit exitfunc(void)
 {
+	int i;
 #ifdef PRINT
 	printk(KERN_INFO "%s: Begin...\n", __func__);
 #endif
-	cdev_del(&mydev->c_dev);
+	for(i = 0; i < nod; i++)
+	{
+		cdev_del(&mydev[i].c_dev);
+	}
+	
 	kfree(mydev);
 	/* extern void unregister_chrdev_region(dev_t, unsigned); */
 	unregister_chrdev_region(dev_id, nod);
 	
 	printk(KERN_INFO "%s: majorNo:%d \n", __func__, majorNo);
-	printk(KERN_INFO "%s: minorNo:%d \n", __func__, minorNo);
 	printk(KERN_INFO "%s: Bye Kernel!!\n", __func__);
 #ifdef PRINT
 	printk(KERN_INFO "%s: End...\n", __func__);
